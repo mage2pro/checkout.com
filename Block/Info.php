fa6d87f761b09ae36f619d5dfbe3d5a60e2354e6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-06 14:03:50 +0300

    no message

--- Block/Info.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Dfe\CheckoutCom\Block;
-/**
- * 2016-05-06
- * @used-by https://code.dmitry-fedyuk.com/m2e/checkout.com/blob/fc9f731/etc/di.xml#L6
- */
-class Info extends \Df\Payment\Block\ConfigurableInfo {}
-
-
