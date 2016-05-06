596eb8a45e86a0e65e2d2fe0eb0619d6b40bfcd8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-06 09:31:23 +0300

    no message

--- Method.php
+++ Method.php
@@ -2,29 +2,22 @@
 namespace Dfe\CheckoutCom;
 use com\checkout\ApiServices\Cards\ResponseModels\Card;
 use com\checkout\ApiServices\Charges\ChargeService;
-use com\checkout\ApiServices\Charges\RequestModels\CardTokenChargeCreate;
-use com\checkout\ApiServices\Charges\ResponseModels\Charge;
+use com\checkout\ApiServices\Charges\ResponseModels\Charge as ChargeResponse;
 use com\checkout\ApiServices\Charges\RequestModels\ChargeCapture;
 use com\checkout\ApiServices\Charges\RequestModels\ChargeVoid;
 use com\checkout\ApiServices\Charges\RequestModels\ChargeRefund;
-use com\checkout\ApiServices\SharedModels\Address as CAddress;
-use com\checkout\ApiServices\SharedModels\Phone as CPhone;
-use com\checkout\ApiServices\SharedModels\Product as CProduct;
 use com\checkout\helpers\ApiHttpClientCustomException as CE;
 use Dfe\CheckoutCom\Settings as S;
 use Dfe\CheckoutCom\Source\Action;
-use Dfe\CheckoutCom\Source\Metadata;
 use Exception as E;
-use libphonenumber\PhoneNumberUtil as PhoneParser;
-use libphonenumber\PhoneNumber as ParsedPhone;
 use Magento\Framework\DataObject;
 use Magento\Framework\Exception\LocalizedException as LE;
 use Magento\Payment\Model\Info;
 use Magento\Payment\Model\InfoInterface;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Address as OrderAddress;
 use Magento\Sales\Model\Order\Creditmemo;
 use Magento\Sales\Model\Order\Invoice;
-use Magento\Sales\Model\Order\Item as OrderItem;
 use Magento\Sales\Model\Order\Payment as OrderPayment;
 use Magento\Sales\Model\Order\Payment\Transaction;
 class Method extends \Df\Payment\Method {
@@ -361,308 +354,15 @@ class Method extends \Df\Payment\Method {
 			else {
 				/** @var \Magento\Sales\Model\Order $order */
 				$order = $payment->getOrder();
-				/** @var \Magento\Sales\Model\Order\Address|null $sa */
-				$sa = $order->getShippingAddress();
-				if (!$sa) {
-					$sa = $order->getBillingAddress();
-				}
-				/** @var \Magento\Store\Model\Store $store */
-				$store = $order->getStore();
-				/** @var string $iso3 */
-				$iso3 = $order->getBaseCurrencyCode();
-				/** @var CardTokenChargeCreate $request */
-				$request = new CardTokenChargeCreate;
-				// 2016-05-03
-				// Не является обязательным, но в целом приятно,
-				// когда в графе «Track ID» значится номер заказа вместо «Unknown».
-				$request->setTrackId($order->getIncrementId());
-				$request->setCustomerName($sa->getName());
-				/**
-				 * 2016-04-21
-				 * «The authorised charge must captured within 7 days
-				 * or the charge will be automatically voided by the system
-				 * and the reserved funds will be released.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/capture-card-charge
-				 *
-				 * «Accepted values either 'y' or 'n'.
-				 * Default is is set to 'y'.
-				 * Defines if the charge will be authorised ('n') or captured ('y').
-				 * Authorisations will expire in 7 days.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 * Несмотря на то, что в документации буквы 'y' и 'n' — прописные,
-				 * в примерах везде используются заглавные.
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#request-example
-				 */
-				$request->setAutoCapture($capture ? 'Y' : 'N');
-				/**
-				 * 2016-04-21
-				 * «Delayed capture time in hours between 0 and 168 inclusive
-				 * that corresponds to 7 days (7x24).
-				 * E.g. 0.5 interpreted as 30 mins.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setAutoCapTime(0);
-				/**
-				 * 2016-04-21
-				 * «A valid charge mode: 1 for No 3D, 2 for 3D, 3 Local Payment.
-				 * Default is 1 if not provided.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 *
-				 * 2016-05-03
-				 * С настройками Личного кабинета по умолчанию
-				 * 3D-Secure будет насильно использоваться для платежей размером не меньше 150 долларов.
-				 */
-				$request->setChargeMode(1);
-				/**
-				 * 2016-04-21
-				 * How are an order's getCustomerEmail() and setCustomerEmail() methods
-				 * implemented and used?
-				 * https://mage2.pro/t/1308
-				 *
-				 * «The email address or customer id of the customer.»
-				 * «Either email or customerId required.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setEmail($order->getCustomerEmail());
-				/**
-				 * 2016-04-23
-				 * Нельзя одновременно устанавливать и email, и customerId.
-				 * Причём товары передаются только при указании email:
-				 * https://github.com/CKOTech/checkout-php-library/blob/7c9312e9/com/checkout/ApiServices/Charges/ChargesMapper.php#L142
-				 */
-				/*if ($order->getCustomerId()) {
-					$request->setCustomerId($order->getCustomerId());
-				} */
-				/** @var array(string => string) $vars */
-				$vars = Metadata::vars($store, $order);
-				/**
-				 * 2016-04-21
-				 * «A description that can be added to this object.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setDescription(df_var(S::s()->description(), $vars));
-				if (is_null($amount)) {
-					$amount = $payment->getBaseAmountOrdered();
-				}
-				/**
-				 * 2016-04-21
-				 * «Expressed as a non-zero positive integer
-				 * (i.e. decimal figures not allowed).
-				 * Divide Bahraini Dinars (BHD), Kuwaiti Dinars (KWD),
-				 * Omani Rials (OMR) and Jordanian Dinars (JOD) into 1000 units
-				 * (e.g. "value = 1000" is equivalent to 1 Bahraini Dinar).
-				 * Divide all other currencies into 100 units
-				 * (e.g. "value = 100" is equivalent to 1 US Dollar).
-				 * Checkout.com will perform the proper conversions for currencies
-				 * that do not support fractional values.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setValue(self::amount($payment, $amount));
-				/**
-				 * 2016-04-21
-				 * «Three-letter ISO currency code
-				 * representing the currency in which the charge was made.
-				 * (refer to currency codes and names)»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setCurrency($iso3);
-				/**
-				 * 2016-04-21
-				 * «Order tracking id generated by the merchant.
-				 * Max length of 100 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setTrackId($payment->getId());
-				/**
-				 * 2016-04-21
-				 * «Transaction indicator. 1 for regular, 2 for recurring, 3 for MOTO.
-				 * Defaults to 1 if not specified.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setTransactionIndicator(1);
-				/**
-				 * 2016-04-21
-				 * «Customer/Card holder Ip.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setCustomerIp($order->getRemoteIp());
-				/**
-				 * 2016-04-21
-				 * «A valid card token (with prefix card_tok_)»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setCardToken($this->iia(self::$TOKEN));
-				foreach ($order->getItems() as $item) {
-					/** @var OrderItem $item */
-					/**
-					 * 2016-03-24
-					 * Если товар является настраиваемым, то
-					 * @uses \Magento\Sales\Model\Order::getItems()
-					 * будет содержать как настраиваемый товар, так и его простой вариант.
-					 */
-					if (!$item->getChildrenItems()) {
-						/** @var CProduct $cProduct */
-						$cProduct = new CProduct;
-						/**
-						 * 2016-04-23
-						 * «Name of product. Max of 100 characters.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						// Простые варианты имеют имена типа «New Very Prive-36-Almond»,
-						// нам удобнее видеть имена простыми,
-						// как у настраиваемого товара: «New Very Prive»).
-						$cProduct->setName(
-							$item->getParentItem()
-							? $item->getParentItem()->getName()
-							: $item->getName()
-						);
-						$cProduct->setProductId($item->getProductId());
-						/**
-						 * 2016-04-23
-						 * «Description of the product.Max of 500 characters.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						$cProduct->setDescription($item->getDescription());
-						/**
-						 * 2016-04-23
-						 * «Stock Unit Identifier.
-						 * Unique product identifier.
-						 * Max length of 100 characters.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						$cProduct->setSku($item->getSku());
-						/**
-						 * 2016-04-23
-						 * «Product price per unit. Max. of 6 digits.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 *
-						 * 2016-05-03
-						 * Не используем здесь @see \Dfe\CheckoutCom\Method::amount(),
-						 * потому что нам в данном случае нужно передавать цену в рублях,
-						 * а не в копейках (в документации об э\том не сказано,
-						 * проверял посредством личного кабинета checkout.com).
-						 */
-						$cProduct->setPrice(df_order_item_price($item));
-						/**
-						 * 2016-04-23
-						 * «Units of the product to be shipped. Max length of 3 digits.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						$cProduct->setQuantity($item->getQtyOrdered());
-						/**
-						 * 2016-04-23
-						 * «image link to product on merchant website.»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						$cProduct->setImage(df_product_image_url($item->getProduct()));
-						/**
-						 * 2016-04-23
-						 * «An array of Product details»
-						 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-						 */
-						$request->setProducts($cProduct);
-					}
-				}
-				/** @var CAddress $rsa */
-				$rsa = new CAddress;
-				/**
-				 * 2016-04-23
-				 * «Address field line 1. Max length of 100 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setAddressLine1($sa->getStreetLine(1));
-				/**
-				 * 2016-04-23
-				 * «Address field line 2. Max length of 100 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setAddressLine2($sa->getStreetLine(2));
-				/**
-				 * 2016-04-23
-				 * «Address postcode. Max. length of 50 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setPostcode($sa->getPostcode());
-				/**
-				 * 2016-04-23
-				 * «The country ISO2 code e.g. US.
-				 * See provided list of supported ISO formatted countries.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setCountry($sa->getCountryId());
-				/**
-				 * 2016-04-23
-				 * «Address city. Max length of 100 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setCity($sa->getCity());
-				/**
-				 * 2016-04-23
-				 * «Address state. Max length of 100 characters.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$rsa->setState($sa->getRegion());
-				/**
-				 * 2016-05-03
-				 * https://github.com/giggsey/libphonenumber-for-php#quick-examples
-				 * @var PhoneParser $phoneParser
-				 */
-				$phoneParser = PhoneParser::getInstance();
-				try {
-					/** @var ParsedPhone $parsedPhone */
-				    $parsedPhone = $phoneParser->parse($sa->getTelephone(), $sa->getCountryId());
-					/** @var CPhone $phone */
-					$phone = new CPhone;
-					/**
-					 * 2016-04-23
-					 * «Contact phone number for the card holder.
-					 * Its length should be between 6 and 25 characters.
-					 * Allowed characters are: numbers, +, (,) ,/ and ' '.»
-					 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-					 */
-					$phone->setNumber($parsedPhone->getNationalNumber());
-					/**
-					 * 2016-04-23
-					 * «Country code for the phone number of the card holder
-					 * e.g. 44 for United Kingdom.
-					 * Please refer to Country ISO and Code section
-					 * in the Other Codes menu option.»
-					 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-					 */
-					$phone->setCountryCode($parsedPhone->getCountryCode());
-					/**
-					 * 2016-04-23
-					 * «Contact phone object for the card holder.
-					 * If provided, it will contain the countryCode and number properties
-					 * e.g. 'phone':{'countryCode': '44' , 'number':'12345678'}.»
-					 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-					 */
-					$rsa->setPhone($phone);
-				} catch (\libphonenumber\NumberParseException $e) {}
-				/**
-				 * 2016-04-23
-				 * «Shipping address details.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setShippingDetails($rsa);
-				/**
-				 * 2016-04-23
-				 * «A hash of FieldName and value pairs e.g. {'keys1': 'Value1'}.
-				 * Max length of key(s) and value(s) is 100 each.
-				 * A max. of 10 KVP are allowed.»
-				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#cardWithTokenTable
-				 */
-				$request->setMetadata(array_combine(
-					dfa_select(Metadata::s()->map(), S::s()->metadata())
-					,dfa_select($vars, S::s()->metadata())
-				));
 				/**
 				 * 2016-04-23
 				 * http://developers.checkout.com/docs/server/api-reference/charges/charge-with-card-token#response
 				 */
-				/** @var Charge $response */
+				/** @var ChargeResponse $response */
 				//xdebug_break();
-			    $response = $this->api()->chargeWithCardToken($request);
+			    $response = $this->api()->chargeWithCardToken(Charge::build(
+					$payment, $this->iia(self::$TOKEN), $amount, $capture
+				));
 				/**
 				 * 2016-05-02
 				 * Иначе операция «void» (отмена авторизации платежа) будет недоступна:
@@ -800,7 +500,7 @@ class Method extends \Df\Payment\Method {
 	 * @param float $amount
 	 * @return int
 	 */
-	private static function amount(InfoInterface $payment, $amount) {
+	public static function amount(InfoInterface $payment, $amount) {
 		/** @var string[] $m1000 */
 		static $m1000 = ['BHD', 'KWD', 'OMR', 'JOD'];
 		/** @var string $iso3 */
