9bd00384ae774696faaa9eb2f05708a48eb1a15b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-06-02 21:45:55 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -4,12 +4,12 @@ use Df\Config\Source\NoWhiteBlack as NWB;
 use Dfe\CheckoutCom\Patch\ApiClient as API;
 use Dfe\CheckoutCom\Source\Prefill;
 use com\checkout\ApiServices\Charges\ChargeService;
-use Magento\Framework\App\ScopeInterface;
+use Magento\Framework\App\ScopeInterface as S;
 class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-05-15
 	 * @param int $customerId
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function actionDesired($customerId, $s = null) {
@@ -20,7 +20,7 @@ class Settings extends \Df\Core\Settings {
 	 * 2016-05-05
 	 * https://github.com/CKOTech/checkout-php-library#example
 	 * https://github.com/CKOTech/checkout-php-library/wiki/Charges#creates-a-charge-with-cardtoken
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return API
 	 */
 	public function api($s = null) {
@@ -34,7 +34,7 @@ class Settings extends \Df\Core\Settings {
 	 * 2016-05-05
 	 * https://github.com/CKOTech/checkout-php-library#example
 	 * https://github.com/CKOTech/checkout-php-library/wiki/Charges#creates-a-charge-with-cardtoken
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return ChargeService
 	 */
 	public function apiCharge($s = null) {return $this->api($s)->chargeService();}
@@ -42,7 +42,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-09
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Description»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function description($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -50,7 +50,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-02-27
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Enable?»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
 	public function enable($s = null) {return $this->b(__FUNCTION__, $s);}
@@ -58,7 +58,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-05-13
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Force 3D-Secure validation for All Customers?»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
 	public function force3DS_forAll($s = null) {return $this->b(__FUNCTION__, $s);}
@@ -68,7 +68,7 @@ class Settings extends \Df\Core\Settings {
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» →
 	 * «Force 3D-Secure validation for the Particular Customer Locations (detected by IP Address)?»
 	 * @param string $countryIso2
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function force3DS_forIPs($countryIso2, $s = null) {
@@ -78,7 +78,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-05-13
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Force 3D-Secure validation for the New Customers?»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
 	public function force3DS_forNew($s = null) {return $this->b(__FUNCTION__, $s);}
@@ -88,7 +88,7 @@ class Settings extends \Df\Core\Settings {
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» →
 	 * «Force 3D-Secure validation for the Particular Shipping Destinations?»
 	 * @param string $countryIso2
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function force3DS_forShippingDestinations($countryIso2, $s = null) {
@@ -98,7 +98,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-14
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Metadata»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string[]
 	 */
 	public function metadata($s = null) {return $this->csv(__FUNCTION__, $s);}
@@ -107,14 +107,14 @@ class Settings extends \Df\Core\Settings {
 	 * 2016-03-09
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Prefill the Payment Form with Test Data?»
 	 * @see \Dfe\CheckoutCom\Source\Prefill::map()
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return array(string => string)|null
 	 */
 	public function prefill($s = null) {return Prefill::s()->config($this->v(__FUNCTION__, $s));}
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function publishableKey($s = null) {
@@ -123,7 +123,7 @@ class Settings extends \Df\Core\Settings {
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	public function secretKey($s = null) {
@@ -133,7 +133,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-14
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Billing Descriptor»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string[]
 	 */
 	public function statement($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -141,7 +141,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Test Mode?»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
 	public function test($s = null) {return $this->b(__FUNCTION__, $s);}
@@ -149,7 +149,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-05-15
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Wait for «Capture» transaction on an order placement if the Payment Action is «Capture»?»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
 	public function waitForCapture($s = null) {return $this->b(__FUNCTION__, $s);}
@@ -164,7 +164,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-15
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Payment Action for a New Customer»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function actionForNew($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -172,7 +172,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-15
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Payment Action for a Returned Customer»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function actionForReturned($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -180,7 +180,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Live Publishable Key»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function livePublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -188,7 +188,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Live Secret Key»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function liveSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
@@ -196,7 +196,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Test Publishable Key»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function testPublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
@@ -204,7 +204,7 @@ class Settings extends \Df\Core\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Checkout.com» → «Test Secret Key»
-	 * @param null|string|int|ScopeInterface $s [optional]
+	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
 	private function testSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
