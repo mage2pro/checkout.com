a7cf5ff45670b80fd6555e197d09e103baeced41
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-07-01 23:00:08 +0300

    no message

--- PlaceOrder.php
+++ PlaceOrder.php
@@ -2,8 +2,8 @@
 namespace Dfe\CheckoutCom;
 use Magento\Checkout\Api\GuestPaymentInformationManagementInterface as IGuest;
 use Magento\Checkout\Api\PaymentInformationManagementInterface as IRegistered;
-use Magento\Sales\Api\Data\OrderInterface;
-use Magento\Sales\Api\Data\OrderPaymentInterface as PaymentInterface;
+use Magento\Quote\Api\Data\AddressInterface as IAddress;
+use Magento\Quote\Api\Data\PaymentInterface as IPayment;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Payment;
 class PlaceOrder {
@@ -11,17 +11,12 @@ class PlaceOrder {
 	 * 2016-05-04
 	 * @param string $cartId
 	 * @param string $email
-	 * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-	 * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
+	 * @param IPayment $paymentMethod
+	 * @param IAddress|null $billingAddress
 	 * @throws \Magento\Framework\Exception\CouldNotSaveException
 	 * @return string
 	 */
-	public function guest(
-		$cartId,
-		$email,
-		\Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-		\Magento\Quote\Api\Data\AddressInterface $billingAddress = null
-	) {
+	public function guest($cartId, $email, IPayment $paymentMethod, IAddress $billingAddress = null) {
 		/** @var IGuest $iGuest */
 		$iGuest = df_o(IGuest::class);
 		return $this->response($iGuest->savePaymentInformationAndPlaceOrder(
@@ -32,16 +27,12 @@ class PlaceOrder {
 	/**
 	 * 2016-05-04
 	 * @param int $cartId
-	 * @param \Magento\Quote\Api\Data\PaymentInterface $paymentMethod
-	 * @param \Magento\Quote\Api\Data\AddressInterface|null $billingAddress
+	 * @param IPayment $paymentMethod
+	 * @param IAddress|null $billingAddress
 	 * @throws \Magento\Framework\Exception\CouldNotSaveException
 	 * @return string
 	 */
-	public function registered(
-		$cartId,
-		\Magento\Quote\Api\Data\PaymentInterface $paymentMethod,
-		\Magento\Quote\Api\Data\AddressInterface $billingAddress = null
-	) {
+	public function registered($cartId, IPayment $paymentMethod, IAddress $billingAddress = null) {
 		/** @var IRegistered $iRegistered */
 		$iRegistered = df_o(IRegistered::class);
 		return $this->response($iRegistered->savePaymentInformationAndPlaceOrder(
