d78d57a79e2cee723609e7cff1f9b58be36afa1e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-12 09:52:22 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -140,8 +140,10 @@ define([
 	/**
 	 * 2016-11-10
 	 * @override
-	 * @see mage2pro/core/Payment/view/frontend/web/card.js
-	 * @used-by card.js::initialize()
+	 * @see Df_Payment/card::prefill()
+	 * https://github.com/mage2pro/core/blob/2.8.2/Payment/view/frontend/web/card.js#L152-L167
+	 * @used-by Df_Payment/card::initialize()
+	 * https://github.com/mage2pro/core/blob/2.8.2/Payment/view/frontend/web/card.js#L134-L137
 	 * @param {*} d 
 	 */
 	prefill: function(d) {
