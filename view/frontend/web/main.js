14a844f651b1dede1c13a33bab95c18d53cff854
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-27 00:36:58 +0300

    1.3.22: «Sometimes getting duplicate orders in checkout»: https://mage2.pro/t/4217

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -91,12 +91,32 @@ define([
 	}),
 	/**
 	 * @override
-	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
-	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
+	 * @see Df_StripeClone/main::placeOrder()
+	 * @used-by Df_Payment/main.html:
+	 *		<button
+	 *			class="action primary checkout"
+	 *			type="submit"
+	 *			data-bind="
+	 *				click: placeOrder
+	 *				,css: {disabled: !isPlaceOrderActionAllowed()}
+	 *				,enable: dfIsChosen()
+	 *			"
+	 *			disabled
+	 *		>
+	 *			<span data-bind="df_i18n: 'Place Order'"></span>
+	 *		</button>
+	 * https://github.com/mage2pro/core/blob/2.9.10/Payment/view/frontend/web/template/main.html#L57-L68
+	 * https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
 	 * @param {this} _this
-	*/
-	placeOrder: function(_this) {
+	 * @param {Event} event
+	 */
+	placeOrder: function(_this, event) {
+		if (event) {
+			event.preventDefault();
+		}
 		if (this.validate()) {
+			// 2017-07-26 «Sometimes getting duplicate orders in checkout»: https://mage2.pro/t/4217
+			this.isPlaceOrderActionAllowed(false);
 			this.initDf().done(function() {
 				/**
 				 * 2016-04-21
@@ -126,9 +146,8 @@ define([
 						// But they are not informative and contain a text like
 						// «Server Operation Failed»
 						// «The last server operation failed.»
-						_this.showErrorMessage(
-							'It looks like you have entered incorrect bank card data.'
-						);
+						_this.showErrorMessage('It looks like you have entered incorrect bank card data.');
+						_this.isPlaceOrderActionAllowed(true);
 					}
 					else {
 						_this.token = response.id;
