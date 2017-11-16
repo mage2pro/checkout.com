a5d410cb195b9fdd1834618620d66fd448b61cfa
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-16 15:59:30 +0300

    1.4.10

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,6 +1,6 @@
 <?php
 namespace Dfe\CheckoutCom\Controller\Index;
-use Df\Framework\Controller\Response\Json;
+use Df\Framework\W\Response\Json;
 use Dfe\CheckoutCom\Handler;
 use Dfe\CheckoutCom\Handler\CustomerReturn;
 class Index extends \Magento\Framework\App\Action\Action {
