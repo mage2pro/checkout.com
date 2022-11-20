941cb7d4d43977098c91a425b49845cdbf966ee9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-20 21:59:18 +0000

    https://github.com/mage2pro/core/issues/174

--- Charge.php
+++ Charge.php
@@ -257,7 +257,6 @@ final class Charge extends \Df\Payment\Charge {
 	/**
 	 * 2016-05-06
 	 * @used-by self::setProducts()
-	 * @param OI $i
 	 */
 	private function cProduct(OI $i) {
 		$r = new CProduct; /** @var CProduct $r */
