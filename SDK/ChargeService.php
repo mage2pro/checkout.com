25af9fecc78251880d5383d90556411ede370cdb
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 05:42:51 +0000

    https://github.com/mage2pro/core/issues/168

--- SDK/ChargeService.php
+++ SDK/ChargeService.php
@@ -1,19 +1,17 @@
 <?php
 namespace Dfe\CheckoutCom\SDK;
-use com\checkout\ApiServices\Charges\ResponseModels\Charge as Response;
+use com\checkout\ApiServices\Charges\ResponseModels\Charge as Res;
 final class ChargeService extends \com\checkout\ApiServices\Charges\ChargeService {
 	/**
 	 * 2016-05-08
 	 * @see \com\checkout\ApiServices\Charges\ChargeService::chargeWithCardToken()
 	 * @param array(string => mixed) $params
 	 */
-	function chargeWithCardTokenDf(array $params):Response {
-		return new Response(ApiHttpClient::postRequest($this->_apiUrl->getCardTokensApiUri(), [
+	function chargeWithCardTokenDf(array $params):Res {return new Res(ApiHttpClient::postRequest(
+		$this->_apiUrl->getCardTokensApiUri(), [
 			'authorization' => $this->_apiSetting->getSecretKey(),
 			'mode' => $this->_apiSetting->getMode(),
 			'postedParam' => $params
-		]));
-	}
+		]
+	));}
 }
-
-
