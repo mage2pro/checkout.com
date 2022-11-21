9e2e2ccaf8ebdc9546977d4c9c6d655b1159b871
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 05:39:49 +0000

    https://github.com/mage2pro/core/issues/174

--- SDK/ApiClient.php
+++ SDK/ApiClient.php
@@ -13,16 +13,11 @@ class ApiClient extends \com\checkout\ApiClient {
 	 * 2016-05-08
 	 * @override
 	 * @see \com\checkout\ApiClient::__construct()
-	 * @param string $secretKey
-	 * @param string $env [optional]
-	 * @param bool $debugMode [optional]
-	 * @param int $connectTimeout
-	 * @param int $readTimeout
 	 */
 	function __construct(
-		$secretKey, $env = 'sandbox' ,$debugMode = false, $connectTimeout = 60, $readTimeout = 60
+		string $secret, string $env = 'sandbox', bool $debug = false, int $connectTimeout = 60, int $readTimeout = 60
 	) {
-		parent::__construct($secretKey, $env ,$debugMode, $connectTimeout, $readTimeout);
+		parent::__construct($secret, $env ,$debug, $connectTimeout, $readTimeout);
 		$this->_chargeService = new ChargeService(AppSetting::getSingletonInstance());
 	}
 
