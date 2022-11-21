6e11cc662c40cf5f1e3f0c3928d5196a2d62430b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-21 05:41:44 +0000

    refactoring

--- SDK/ApiHttpClient.php
+++ SDK/ApiHttpClient.php
@@ -4,15 +4,9 @@ namespace Dfe\CheckoutCom\SDK;
 class ApiHttpClient {
 	/**
 	 * 2016-08-05
-	 * @param array(string => string) $payload [optional]
+	 * @param array(string => string) $d [optional]
 	 */
-	static function postRequest(string $uri, array $payload = []):\CheckoutApi_Lib_RespondObj {
-		return self::api()->request($uri, ['method' => 'POST'] + $payload, true);
-	}
-
-	/**
-	 * 2016-08-05
-	 * @return \CheckoutApi_Client_Client|\CheckoutApi_Client_ClientGW3
-	 */
-	private static function api() {return \CheckoutApi_Api::getApi();}
+	static function postRequest(string $uri, array $d = []):\CheckoutApi_Lib_RespondObj {return
+		\CheckoutApi_Api::getApi()->request($uri, ['method' => 'POST'] + $d, true)
+	;}
 }
